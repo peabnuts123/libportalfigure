eef8ec0f2fe24f74acc2a13f96b7dbefb44417a3
peabnuts123 <peabnuts123@gmail.com>
2025-11-28 12:22:55 +1300

    Fix incorrect emitted package type in JavaScript backend

--- src/javascript.rs
+++ src/javascript.rs
@@ -37,7 +37,7 @@ pub fn emit(all_figure_data: &Vec<FigureData>, version: &str) {
   "keywords": [],
   "author": "peabnuts123",
   "license": "CC0-1.0",
-  "type": "commonjs"
+  "type": "module"
 }}"#
     );
     std::fs::write(format!("{package_dir}/package.json"), package_json_content)
