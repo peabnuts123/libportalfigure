3a7f9f88990ca6e0a4403e7a7a4e881305e72ae4
peabnuts123 <peabnuts123@gmail.com>
2025-11-28 12:22:27 +1300

    Emit README in javascript and rust backends

--- src/rust.rs
+++ src/rust.rs
@@ -11,11 +11,11 @@ pub fn emit(all_figure_data: &Vec<FigureData>, version: &str) {
 
     // Config and utils
     let exported_vec_name = "PORTAL_FIGURES";
-    let figure_data_to_export_name =
-        |figure_data: &FigureData| figure_data.name_id.to_uppercase();
+    let figure_data_to_export_name = |figure_data: &FigureData| figure_data.name_id.to_uppercase();
 
     // Cargo.toml
-    let cargo_toml_content = format!(r#"[package]
+    let cargo_toml_content = format!(
+        r#"[package]
 name = "portal_figure"
 description = "A code library of data relating to figures from the Skylanders series of video games."
 version = "{version}"
@@ -23,7 +23,8 @@ edition = "2024"
 license = "CC0-1.0"
 
 [dependencies]
-"#);
+"#
+    );
     std::fs::write(format!("{crate_dir}/Cargo.toml"), cargo_toml_content)
         .expect("Failed to write Cargo.toml");
 
@@ -86,4 +87,55 @@ pub fn find_figure(figure_id: u16, variant_id: u16) -> Option<&'static PortalFig
 "#
     );
     std::fs::write(format!("{}/lib.rs", src_dir), lib_rs_content).expect("Failed to write lib.rs");
+
+    // README.md
+    let readme_content = r#"# portal_figure
+
+A library of data relating to figures from the Skylanders series of video games.
+
+This crate is built from source: https://github.com/peabnuts123/libportalfigure
+
+## Installation
+
+```sh
+cargo add portal_figure
+```
+
+## Usage
+
+```rust
+use portal_figure::{PORTAL_FIGURES, PortalFigure, SPYRO, find_figure};
+
+fn main() {
+    // Use `find_figure()` to look up figure data by Figure ID + Variant ID
+    let figure: &PortalFigure = find_figure(0x1ce, 0x3000).expect("Couldn't find figure");
+    println!(
+        "{} (figure_id='0x{:x}') (variant_id='0x{:x}')",
+        figure.name, figure.figure_id, figure.variant_id
+    );
+
+    // Individually exported figure data
+    println!(
+        "Spyro (figure_id='0x{:x}') (variant_id='0x{:x}')",
+        SPYRO.figure_id, SPYRO.variant_id
+    );
+
+    // `PORTAL_FIGURES` is a &[PortalFigure] of all figures
+    let all_spyros = PORTAL_FIGURES
+        .iter()
+        .filter(|figure| figure.name.to_lowercase().contains("spyro"))
+        .collect::<Vec<&PortalFigure>>();
+
+    println!("All Spyro figures:");
+    for figure in all_spyros {
+        println!(
+            "{} (figure_id='0x{:x}') (variant_id='0x{:x}')",
+            figure.name, figure.figure_id, figure.variant_id
+        );
+    }
+}
+```
+"#;
+    std::fs::write(format!("{}/README.md", crate_dir), readme_content)
+        .expect("Failed to write README.md");
 }
